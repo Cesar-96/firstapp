ac8eefe31c8caad54d1255dc081e2fc9047061de
Luis Yucra <luis.yucra@gmail.com>
2018-08-25 20:22:48 -0500

    Add another project

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        // Alt + 9
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello");
+        }
+    }
+}
