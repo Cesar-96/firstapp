dd2f15bf5906a35307a6b2c4e410c5850e48444d
Pablo Yucra <cesar.yucra12@gmail.com>
2018-08-25 19:58:07 -0500

    first one

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int opcion = 0;
+            double weightOnEarth = 0.0;
+            double weightOnOther = 0.0;
+            string dato = "";
+
+            Console.WriteLine("1. Mercurio, 2. Venus 3. Marte");
+            dato = Console.ReadLine();
+            opcion = Convert.ToInt32(dato);
+
+            Console.WriteLine("Give me your weight on earth");
+            dato = Console.ReadLine();
+            weightOnEarth = Convert.ToDouble(dato);
+
+            switch (opcion)
+            {
+                case 1:
+                    weightOnOther = weightOnEarth * 0.38;
+                    break;
+                case 2:
+                    weightOnOther = weightOnEarth * 0.91;
+                    break;
+                case 3:
+                    weightOnOther = weightOnEarth * 0.48;
+                    break;
+            }
+            Console.WriteLine("Your weight on that planet is {}", weightOnOther);
+        }
+
+    }
+}
